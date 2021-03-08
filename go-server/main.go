a70e4c510c753b90244a0775534c62de0ff2ce5c
Anish <sunaranish17@gmail.com>
2021-03-08 19:31:04 +0545

    updates on main file

--- go-server/main.go
+++ go-server/main.go
@@ -1,13 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"recaptcha-go/src/controller"
+	"recaptcha-go/src/utils"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	utils.LoadEnv()
 	r := gin.Default()
+	r.Use(cors.Default())
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+	r.POST("/verify-recaptcha", controller.VerifyReCaptcha)
 	r.Run()
 }
